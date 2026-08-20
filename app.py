038f8b2686b2efa1ace9678a565fc898722c92b9
belencajahuamanhinostroza-lab <belencajahuamanhinostroza@gmail.com>
2026-08-20 13:47:17 -0500

    app.py

--- app.py
+++ app.py
@@ -11,23 +11,23 @@ st.image(image, caption='Revisate la vista')
 texto = st.text_input('Los ojos son una parte importante de la salud.', '¿Qué importancia tiene el cuidado de la vista?')
 st.write('Algunas enfermedades oculares pueden provocar pérdida de la visión sin señales de advertencia tempranas, por lo que es importante detectarlas lo antes posible. Revise sus ojos con la frecuencia que le recomiende su profesional de la salud o si nota nuevos problemas de visión.')
 
-st.subheader("Ahora usemos 2 Columnas")
+st.subheader("¿Cómo puedo mantener mis ojos sanos?")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.subheader("Esta es la primera columna")
-    st.write("Las interfaces multimodales mejoran la experiencia de usuario")
-    resp = st.checkbox('Estoy de acuerdo')
+    st.subheader("Prevención y factores de riesgo")
+    st.write("Dieta, nutrición y suplementos para la salud ocular (Academia Americana de Oftalmología), "Gafas de seguridad: Cómo prevenir lesiones oculares que pueden causar ceguera (Academia Americana de Oftalmología)")
+    resp = st.checkbox('Recibir inoformación')
     if resp:
        st.write('Correcto!')
   
 with col2:
-    st.subheader("Esta es la segunda columna")
-    modo = st.radio("Que Modalidad es la principal en tu interfaz", ('Visual', 'auditiva', 'Táctil'))
-    if modo == 'Visual':
-       st.write('La vista es fundamental para tu interfaz')
-    if modo == 'auditiva':
-       st.write('La audición es fundamental para tu interfaz')
-    if modo == 'Táctil':
-       st.write('El tacto es fundamental para tu interfaz')
+    st.subheader("¿Cómo puedo mantener mis ojos sanos?")
+    modo = st.radio("Hay muchas cosas que usted puede hacer para proteger sus ojos y ver lo mejor posible:", ('Dele un descanso a sus ojos', 'Use gafas de sol', 'Evite frotarse los ojos'))
+    if modo == 'Dele un descanso a sus ojos':
+       st.write('Pasar muchas horas frente a la computadora u otras pantallas digitales puede hacer que parpadee menos,')
+    if modo == 'Use gafas de sol':
+       st.write('Protéjase los ojos usando gafas de sol que bloqueen entre el 99 y el 100% de la radiación UV-A y UV-B')
+    if modo == 'Evite frotarse los ojos':
+       st.write('Frotarse los ojos puede transferir suciedad y bacterias que pueden causar irritación o provocar una infección')
