8659ba9241ba99acdaccb03ff2f477d63c7e3bea
belencajahuamanhinostroza-lab <belencajahuamanhinostroza@gmail.com>
2026-08-20 13:16:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-import streamlit as st
+iimport streamlit as st
+from PIL import Image
 
-st.title("HOLA!! MI NOMBRE ES BELEN")
+st.title(" Mi Primera App!!")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('paolasuarez.jpg')
+st.image(image, caption='Interfaces multimodales')
