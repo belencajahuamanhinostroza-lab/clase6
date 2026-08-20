721af71804a1d0eec7303cf41abfe591f86d6ca8
belencajahuamanhinostroza-lab <belencajahuamanhinostroza@gmail.com>
2026-08-20 13:28:40 -0500

    app.py

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfac
 st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('paolasuarez.jpg')
 st.image(image, caption='Interfaces multimodales')
+
+texto = st.text_input('Una interfaz multimodal es un sistema digital que permite a los usuarios interactuar usando dos o más métodos de comunicación a la vez, como la voz, el tacto, los gestos y la mirada. Estos canales se combinan para dar una experiencia más natural.', 'Este es mi texto')
+st.write('El texto escrito es', texto)
